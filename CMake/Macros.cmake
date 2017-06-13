99bf97d40bc30efa00e286adb3e088afd6b4d2b7
Thibault Buathier <thibault.buathier@gmail.com>
2017-06-13 11:33:01 +0200

    Remove test for Android cross-compilation

--- CMake/Macros.cmake
+++ CMake/Macros.cmake
@@ -66,30 +66,31 @@ macro(CURL_INTERNAL_TEST_RUN CURL_TEST)
       set(CURL_TEST_ADD_LIBRARIES
         "-DLINK_LIBRARIES:STRING=${CMAKE_REQUIRED_LIBRARIES}")
     endif(CMAKE_REQUIRED_LIBRARIES)
-
-    message(STATUS "Performing Curl Test ${CURL_TEST}")
-    try_run(${CURL_TEST} ${CURL_TEST}_COMPILE
-      ${CMAKE_BINARY_DIR}
-      ${CMAKE_CURRENT_SOURCE_DIR}/CMake/CurlTests.c
-      CMAKE_FLAGS -DCOMPILE_DEFINITIONS:STRING=${MACRO_CHECK_FUNCTION_DEFINITIONS}
-      "${CURL_TEST_ADD_LIBRARIES}"
-      OUTPUT_VARIABLE OUTPUT)
-    if(${CURL_TEST}_COMPILE AND NOT ${CURL_TEST})
-      set(${CURL_TEST} 1 CACHE INTERNAL "Curl test ${FUNCTION}")
-      message(STATUS "Performing Curl Test ${CURL_TEST} - Success")
-    else(${CURL_TEST}_COMPILE AND NOT ${CURL_TEST})
-      message(STATUS "Performing Curl Test ${CURL_TEST} - Failed")
-      set(${CURL_TEST} "" CACHE INTERNAL "Curl test ${FUNCTION}")
-      file(APPEND "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log"
-        "Performing Curl Test ${CURL_TEST} failed with the following output:\n"
-        "${OUTPUT}")
-      if(${CURL_TEST}_COMPILE)
-        file(APPEND
-          "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log"
-          "There was a problem running this test\n")
-      endif(${CURL_TEST}_COMPILE)
-      file(APPEND "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log"
-        "\n\n")
-    endif(${CURL_TEST}_COMPILE AND NOT ${CURL_TEST})
+    if(OS STREQUAL android)
+      message(STATUS "Performing Curl Test ${CURL_TEST}")
+      try_run(${CURL_TEST} ${CURL_TEST}_COMPILE
+        ${CMAKE_BINARY_DIR}
+        ${CMAKE_CURRENT_SOURCE_DIR}/CMake/CurlTests.c
+        CMAKE_FLAGS -DCOMPILE_DEFINITIONS:STRING=${MACRO_CHECK_FUNCTION_DEFINITIONS}
+        "${CURL_TEST_ADD_LIBRARIES}"
+        OUTPUT_VARIABLE OUTPUT)
+      if(${CURL_TEST}_COMPILE AND NOT ${CURL_TEST})
+        set(${CURL_TEST} 1 CACHE INTERNAL "Curl test ${FUNCTION}")
+        message(STATUS "Performing Curl Test ${CURL_TEST} - Success")
+      else(${CURL_TEST}_COMPILE AND NOT ${CURL_TEST})
+        message(STATUS "Performing Curl Test ${CURL_TEST} - Failed")
+        set(${CURL_TEST} "" CACHE INTERNAL "Curl test ${FUNCTION}")
+        file(APPEND "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log"
+          "Performing Curl Test ${CURL_TEST} failed with the following output:\n"
+          "${OUTPUT}")
+        if(${CURL_TEST}_COMPILE)
+          file(APPEND
+            "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log"
+            "There was a problem running this test\n")
+        endif(${CURL_TEST}_COMPILE)
+        file(APPEND "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log"
+          "\n\n")
+      endif(${CURL_TEST}_COMPILE AND NOT ${CURL_TEST})
+    endif(OS STREQUAL android)
   endif()
 endmacro(CURL_INTERNAL_TEST_RUN)
